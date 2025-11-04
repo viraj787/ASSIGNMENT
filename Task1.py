bc90c47f804f6f3feea3a93b1598d1f869e6f57f
viraj787 <154401306+viraj787@users.noreply.github.com>
2025-11-04 12:00:33 +0530

    Assignment 1

---
+++ Task1.py
@@ -0,0 +1,9 @@
+# #Task 1
+a = int(input("Enter the first number :"))
+b = int(input("Enter the second number :"))
+
+print("\n5Addition :",a+b)
+print("Subtraction :",a-b)
+print("Multiplication :",a*b)
+print("Division :",a/b)
+
---
+++ Task2.py
@@ -0,0 +1,6 @@
+
+#Task 2 
+first_name = input("Enter the first name:")
+last_name = input("Enter the last name:")
+
+print(f"Hello,{first_name} {last_name}! Welcome to python program")
